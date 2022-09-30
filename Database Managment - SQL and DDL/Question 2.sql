1fc03c7a2644ece248a98151c2fd476e176ff05c
Roni Fridman <80967231+RoniFridman@users.noreply.github.com>
2022-09-30 14:59:15 +0300

    Delete Database Managment - SQL and DDL directory

--- Database Managment - SQL and DDL/Question 2.sql
+++
@@ -1,5 +0,0 @@
-SELECT tID1 AS tID, name, COUNT(*) AS friendsNum
-FROM Friends INNER JOIN Traveler on Friends.tID1 = Traveler.tID
-GROUP BY tID1, name
-HAVING COUNT(tID1) >= 5
-ORDER BY friendsNum DESC, tID1 DESC;
